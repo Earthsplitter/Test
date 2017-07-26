591ea58e80695fc159e4474acdca292a3ad7ddb4
Ming Wen <wmcs@mail.ustc.edu.cn>
2017-07-26 12:17:03 +0800

    feat: 1

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,134 @@
+<!--<template>-->
+  <!--<div id="app">-->
+    <!--<h1>{{title}}</h1>-->
+    <!--<v-button status="primary">Hi</v-button>-->
+    <!--<v-table :data="data">-->
+      <!--<v-table-column v-for="column in headers" :key="column.header"-->
+        <!--:label="column.header" :column-key="column.source"-->
+        <!--&gt;</v-table-column>-->
+    <!--</v-table>-->
+
+  <!--</div>-->
+<!--</template>-->
+
+<!--<script>-->
+
+  <!--export default {-->
+    <!--name: 'app',-->
+    <!--data () {-->
+        <!--return {-->
+          <!--// 填入管理端标题-->
+          <!--title: '标签管理后台',-->
+          <!--// 填入表格头-->
+          <!--headers: [-->
+            <!--{ header: '标签名', type: 'editable', source: 'name'},-->
+            <!--{ header: '关键字', type: 'editable', source: 'keyword'},-->
+            <!--{ header: '日期', type: 'editable', source: 'date'}-->
+          <!--],-->
+          <!--data: [-->
+            <!--{name: '1', keyword: '1', date: '1'}-->
+          <!--]-->
+        <!--}-->
+    <!--}-->
+  <!--}-->
+<!--</script>-->
+
+<!--<style>-->
+<!--#app {-->
+  <!--font-family: 'Avenir', Helvetica, Arial, sans-serif;-->
+  <!-- -webkit-font-smoothing: antialiased;-->
+  <!-- -moz-osx-font-smoothing: grayscale;-->
+  <!--color: #2c3e50;-->
+  <!--margin-top: 20px;-->
+  <!--display: flex;-->
+  <!--flex-direction: column;-->
+  <!--align-items: center;-->
+<!--}-->
+<!--</style>-->
+
 <template>
-  <div id="app">
-    <h1>{{title}}</h1>
-    <v-table :data="data" :pagination="pagination">
-      <v-table-column v-for="column in headers" :key="column.header"
-        :label="column.header" :column-key="column.source"
-        ></v-table-column>
-    </v-table>
+  <div>
+    <h1>H</h1>
+    <v-button status="primary">Hi</v-button>
+  <v-table
+    :data="data1">
+    <v-table-column v-for="column in columns1" :key="column.label"
+                    :column-key="column.columnKey"
+                    :label="column.label"
+                    :align="column.align"
+                    :sortable="column.sortable"
+                    :width="column.width"
+                    :formatter="column.formatter"
+    ></v-table-column>
+    <v-table-column align="right">
+      <template slot="cell" scope="props">
+        <a @click.stop="deleteRow(props.index)" class="table-delete">删除</a>
+      </template>
+    </v-table-column>
+  </v-table>
   </div>
-</template>
+ </template>
 
 <script>
-  import VTable from "../node_modules/vhtml-ui/packages/table/src/table";
-  import VTableColumn from "../node_modules/vhtml-ui/packages/table/src/table-column";
+  import VButton from "../node_modules/vhtml-ui/packages/button/src/button";
+  const data = [];
+  for (let i = 0; i < 10; ++i) {
+    data.push({
+      name: 'erwin',
+      date: +new Date(),
+      math: parseInt(Math.random() * 100),
+      english: parseInt(Math.random() * 100),
+      cpp: parseInt(Math.random() * 100),
+      sql: parseInt(Math.random() * 100)
+    });
+  }
+
+  const columns = [{
+    label: '姓名',
+    columnKey: 'name',
+    width: 200,
+    expandable: true
+  },
+    {
+      label: '日期',
+      columnKey: 'date',
+      sortable: true,
+      formatter: function (value) {
+        return new Date(value).toString().substr(0, 15);
+      }
+    },
+    {
+      label: '数学',
+      columnKey: 'math',
+      align: 'right',
+      width: 100
+    },
+    {
+      label: '英语',
+      columnKey: 'english',
+      align: 'right',
+      sortable: true,
+      width: 100
+    },
+    {
+      label: 'C++',
+      columnKey: 'cpp',
+      align: 'right',
+      width: 100
+    }]
 
   export default {
-    components: {VTableColumn, VTable, VButton},
-    name: 'app',
-    data () {
-        return {
-          // 填入管理端标题
-          title: '标签管理后台',
-          // 填入表格头
-          headers: [
-            { header: '标签名', type: 'editable', source: 'name'},
-            { header: '关键字', type: 'editable', source: 'keyword'},
-            { header: '日期', type: 'editable', source: 'date'}
-          ],
-          data: [
-            {name: '1', keyword: '1', date: '1'}
-          ]
-        }
+    components: {VButton}, data() {
+      return {
+        columns1: columns,
+        data1: data,
+      };
+    },
+    methods: {
+      deleteRow(index) {
+        console.log(index);
+        this.data1.splice(index, 1);
+      }
     }
   }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,16 @@
-// // The Vue build version to load with the `import` command
-// // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// import App from './App'
-// import 'vhtml-ui/lib/vhtml.css'
-//
-// Vue.config.productionTip = false
-//
-// /* eslint-disable no-new */
-// new Vue({
-//   el: '#app',
-//   template: '<App/>',
-//   components: { App }
-// })
+import App from './App'
+import 'vhtml-ui/lib/vhtml.css'
+import VHtml from 'vhtml-ui'
+Vue.use(VHtml)
 
-Vue.component('jsx-example', {
-  render (h) { // <-- h must be in scope
-    return <div id="foo">bar</div>
-  }
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
 })
